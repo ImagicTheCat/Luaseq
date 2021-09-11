917dc63c1ed16f247b767e912bf94419b1b8bd99
ImagicTheCat <imagicthecat@gmail.com>
2021-09-11 14:44:10 +0200

    Improve error handling and return async coroutine.
    Propagate the coroutine resume error by displaying that error first
    (there may be a better way to do this by merging the two tracebacks).

--- src/Luaseq.lua
+++ src/Luaseq.lua
@@ -27,8 +27,7 @@ SOFTWARE.
 
 local Luaseq = {}
 
-local select = select
-local error = error
+local select, error, ipairs = select, error, ipairs
 local setmetatable = setmetatable
 local table_unpack = table.unpack or unpack
 local table_insert = table.insert
@@ -41,28 +40,29 @@ local stderr = io.stderr
 
 -- Task
 
--- wait for task to return
--- (will yield the current coroutine if the task is not completed)
+-- Wait for task completion.
+-- Will yield the current coroutine if the task is not completed.
+--
 -- return task return values
 local function task_wait(self)
   if self.r then return unpack(self.r,1,self.n) end -- already done, return values
-
   local co = coroutine_running()
   if not co then error("async wait outside a coroutine") end
   table_insert(self, co)
   return coroutine_yield(co) -- wait for the task to return
 end
 
--- return/complete/end task
--- (multiple calls will do nothing)
--- ...: return values
+-- Complete task. Subsequent calls will have no effect.
+-- ...: task return values
 local function task_return(self, ...)
   if not self.r then
     self.r, self.n = {...}, select("#", ...)
-
     for _, co in ipairs(self) do
       local ok, err = coroutine_resume(co, ...)
-      if not ok then stderr:write(debug_traceback(co, "async: "..err).."\n") end
+      if not ok then
+        stderr:write(debug_traceback(co, "async: "..err).."\n")
+        error("error resuming coroutine")
+      end
     end
   end
 end
@@ -74,16 +74,24 @@ local meta_task = {
 
 -- Luaseq
 
--- no parameters: create a task
+-- Async utility.
+--
+-- no arguments: create a task
 --- return task
--- parameters: execute function as coroutine (shortcut)
+--
+-- with arguments: execute function as a coroutine (directly resumed)
 --- f: function
+--- return created coroutine (thread)
 function Luaseq.async(f)
-  if f then
+  if f then -- create coroutine
     local co = coroutine_create(f)
     local ok, err = coroutine_resume(co)
-    if not ok then stderr:write(debug_traceback(co, "async: "..err).."\n") end
-  else
+    if not ok then
+      stderr:write(debug_traceback(co, "async: "..err).."\n")
+      error("error resuming coroutine")
+    end
+    return co
+  else -- create task
     return setmetatable({}, meta_task)
   end
 end
